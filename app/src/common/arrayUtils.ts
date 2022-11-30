1b22df8c8bbe36631cc5e788e47dd9e3a4ada406
Balint Nagy <b.nagy.balint@gmail.com>
2022-11-30 12:49:37 +0100

    added ungroup()

--- app/src/common/arrayUtils.ts
+++ app/src/common/arrayUtils.ts
@@ -3,6 +3,7 @@ export function groupBy<K, T>(arr: T[], keyFn: (item: T) => K): Map<K,T[]> {
      * Groups array elements using a grouping callback.
      * 
      * @param arr The array to be grouped
+     * @param keyFn The grouping logic.
      * @returns The array items grouped into a Map object.
      */
     return arr.reduce((m: Map<K,T[]>, item: T) => {
@@ -14,3 +15,17 @@ export function groupBy<K, T>(arr: T[], keyFn: (item: T) => K): Map<K,T[]> {
         return m;
     }, new Map<K,T[]>());
 }
+
+export function ungroup<K, T>(m: Map<K,T[]>): T[] {
+    /**
+     * Ungroups a map, flattening the items of the groups into a single array.
+     * NOTE: due to the implementation-defined ordering of maps, there are no guarantees
+     * on the order of the concatenation of the sub-lists. However, sub-list ordering is
+     * preserved.
+     * 
+     * @param m The map to be grouped
+     * @returns The array items grouped into a Map object.
+     */
+    return Array.from(m.values()).flat();
+}
+
