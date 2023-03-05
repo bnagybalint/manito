8fd8a70f7ad0a442877852ff6026856651517a1a
Balint Nagy <b.nagy.balint@gmail.com>
2023-03-05 15:44:49 +0100

    added DateRangeFilter

---
+++ src/lib/js/manito/core-ui-components/src/DateRangeFilter/DateRangeFilter.stories.tsx
@@ -0,0 +1,30 @@
+import { StoryFn, Meta } from '@storybook/react';
+import moment from 'moment';
+
+import DateRangeFilter from './DateRangeFilter';
+
+export default {
+    title: 'DateRangeFilter',
+    component: DateRangeFilter,
+    argTypes: {
+        startDate: { type: 'string' },
+        endDate: { type: 'string' },
+        onDateRangeChange: { action: 'date range updated' },
+        onPreviousClick: { action: 'changed to previous interval' },
+        onNextClick: { action: 'changed to next interval' },
+    }
+} as Meta<typeof DateRangeFilter>;
+
+const Template: StoryFn<typeof DateRangeFilter> = (args) => (<DateRangeFilter {...args} />);
+
+export const Default = Template.bind({});
+Default.args = {
+    startDate: moment("2023-02-12"),
+    endDate: moment("2023-02-15"),
+};
+
+export const CurrentMonth = Template.bind({});
+CurrentMonth.args = {
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month'),
+};
